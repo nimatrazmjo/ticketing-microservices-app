e9627f1851e2aeb435f65b55a74bc9eeef76d865
Nimatullah Razmjo <nimat.razmjo@gmail.com>
2021-12-22 12:45:44 +0430

    Setup skaffold for local

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRounter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRounter } from './routes/signout';
@@ -15,13 +17,33 @@ app.use(currentUserRounter);
 app.use(signinRouter);
 app.use(signoutRounter);
 app.use(signupRouter);
-console.log('calling')
+
 app.all('*',async () => {
-  console.log('Called');
   throw new NotFoundError();
 });
 app.use(ErrorHandler);
 
+const start = () => {
+  try {
+    console.log('hereeeee')
+     mongoose.connect(`mongodb://auth-mongo-service:27017/auth`
+    // ,
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true
+    // }
+    ); 
+    console.log('Connected to mongo DB');
+  } catch (error) {
+    console.error(error,'error');
+  }
+}
+
 app.listen(3000, () => {
+
+  
   console.log('Listening on port', 3000);
 });
+
+start();
