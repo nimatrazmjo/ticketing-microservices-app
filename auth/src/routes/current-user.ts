6e012985fcf0f6dfea5d2957967c24824d8e8d1e
Nimatullah Razmjo <nimat.razmjo@gmail.com>
2021-12-26 16:07:46 +0430

    Add current-user and signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,18 @@
 
-import { Router } from "express";
-
+import { Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
 const router = Router();
 
-router.get('/api/users/current-user', (req, res) => {
-  res.send('Hell, This is nimatullah razmjo!');
+router.get('/api/users/current-user', (req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+      res.send({currentUser: null});
+    }
+    try {
+      const payload = jwt.verify(req.session?.jwt,process.env.JWT_KEY!);
+      res.send({currentUser:payload});  
+    } catch (error) {
+      res.send({currentUser: null});
+    }
 });
 
 export {router as currentUserRounter};
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/api/users/signout', (req, res) => {
- res.send("Jello");
+ req.session = null;
+ res.send({});
 });
 
 export {router as signoutRounter};
